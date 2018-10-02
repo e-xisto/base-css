acf088b15df046d94289a91699960f3721d244a4
Alejandro <atrinidad@existo.es>
2018-10-02 19:09:43 +0200

    dropdown javascript

--- src/js/components/dropdown.ts
+++ src/js/components/dropdown.ts
@@ -1,44 +1,66 @@
 
 function init() {
 
-	document.addEventListener('click', (event: MouseEvent) => {
+
+	document.addEventListener('click', (event: Event) => {
 		let item = <Element>event.target;
 		if (item.classList.contains('dropdown-toggle')) {
-			toggle(event, item.closest('.dropdown'), item);
+			event.preventDefault();
+			event.stopPropagation();
+			toggle(item.closest('.dropdown'), item);
 		}
 	});
-
-
-
 
 }
 
 
-function toggle(event: MouseEvent, container: Element | null, button: Element) {
+function toggle(container: Element | null, button: Element) {
 
-	event.preventDefault();
 	if (container) {
 		let menus = container.getElementsByClassName('dropdown-menu');
 		Array.from(menus).forEach((menu) => {
-			if (!menu.classList.contains('show'))
-				show(container, button, menu);
-			else hide(container, button, menu);
+			if (menu.classList.contains('show')) hide(container, button, menu);
+			else show(container, button, menu);
 		});
 	}
 }
 
 
 function show(container: Element, button: Element, menu: Element) {
+
 	button.classList.add('active');
 	menu.classList.add('show');
-	menu.setAttribute('tabindex', '-1');
-	// menu.focus();
+
+	container.setAttribute('tabindex', '-1');
+	(<HTMLElement>container).focus();
+	container.addEventListener('blur', menuBlurEventHandler);
 }
 
+
 function hide(container: Element, button: Element, menu: Element) {
+
 	button.classList.remove('active');
 	menu.classList.remove('show');
-	menu.removeAttribute('tabindex');
+
+	container.removeAttribute('tabindex');
+	container.removeEventListener('blur', menuBlurEventHandler);
+}
+
+
+function menuBlurEventHandler(event: Event) {
+
+	let container = <Element>event.target;
+	container.removeAttribute('tabindex');
+console.log('blur');
+	let menus = container.getElementsByClassName('dropdown-menu');
+	Array.from(menus).forEach((menu) => {
+		menu.classList.remove('show');
+	});
+
+	let toggles = container.getElementsByClassName('dropdown-toggle');
+	Array.from(toggles).forEach((toggle) => {
+		toggle.classList.remove('active');
+	});
 }
 
 
