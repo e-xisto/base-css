1b207bb8956e0a4d97fccc5dfda7b8d35a45c465
Alejandro <atrinidad@existo.es>
2019-04-03 17:05:42 +0200

    revisión ellipsis

--- src/js/base.ts
+++ src/js/base.ts
@@ -3,7 +3,7 @@ import tabs from './components/tabs';
 import modal from './components/modal';
 import offcanvas from './components/offcanvas';
 import collapse from './components/collapse';
-import dotdotdot from './components/dotdotdot';
+import ellipsis from './components/ellipsis';
 
 
 dropdown.init();
@@ -11,4 +11,4 @@ tabs.init();
 modal.init();
 offcanvas.init();
 collapse.init();
-dotdotdot.init();
+ellipsis.init();
---
+++ src/js/components/ellipsis.ts
@@ -0,0 +1,54 @@
+
+let ellipsis = new Array<HTMLElement>();
+
+function init() {
+	let elements = Array.from(document.getElementsByClassName('ellipsis'));
+	for (let element of elements) {
+		ellipsis.push(<HTMLElement>element);
+	}
+
+	document.addEventListener('resize', cutElements);
+	cutElements();
+}
+
+function cutElements() {
+	for (let element of ellipsis) {
+		if (element.getAttribute('data-ellipsis')) {
+			cutLines(element, Number(element.getAttribute('data-ellipsis')));
+		}
+		if (element.getAttribute('data-ellipsis-height')) {
+			cutHeight(element, Number(element.getAttribute('data-ellipsis-height')));
+		}
+	}
+}
+
+function cutHeight(element: HTMLElement, pixels: number) {
+
+	if (isNaN(pixels)) return;
+	let lineHeight = parseInt(window.getComputedStyle(element).lineHeight || '24px');
+
+	let lines = Math.floor(pixels / (lineHeight)) || 1;
+	let height = lines * lineHeight;
+	let actualHeight = parseInt(window.getComputedStyle(element).height || '');
+	if (actualHeight >= height) {
+		element.style.height = height + 'px';
+		(<any>element.style)['-webkit-line-clamp'] = lines;
+	}
+}
+
+
+function cutLines(element: HTMLElement, lines: number) {
+
+	let lineHeight = parseInt(window.getComputedStyle(element).lineHeight || '24px');
+
+	let height = lines * lineHeight;
+	let actualHeight = parseInt(window.getComputedStyle(element).height || '');
+
+	if (actualHeight >= height) {
+		element.style.height = height + 'px';
+		(<any>element.style)['-webkit-line-clamp'] = lines;
+	}
+}
+
+
+export default { init };
